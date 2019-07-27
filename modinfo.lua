f75a72d5d32af0de39e35608148eb81395ed7d31
Gajo Petrovic <gajo01@gmail.com>
2019-07-27 17:09:19 +0900

    add modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+  name = 'BYAR Chobby',
+  shortName = 'BYAR_CHOBBY',
+  description = 'BYAR mutator for Chobby',
+  version = '$VERSION',
+  mutator = 'Official',
+  modtype = 5,
+  onlyLocal = true,
+  depend = {
+      'rapid://chobby:test',
+  },
+}
