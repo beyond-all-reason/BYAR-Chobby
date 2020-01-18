1349286f75cd6fcab190cdd0457a3af6310868b0
Floris <floris@ruwhof.net>
2020-01-18 19:59:59 +0100

    resolution limitor fix

--- LuaMenu/widgets/gfx_resolution_limiter.lua
+++ LuaMenu/widgets/gfx_resolution_limiter.lua
@@ -15,21 +15,26 @@ function checkResolution()
 	-- resize resolution if is larger than screen resolution
 	local vsx,vsy = Spring.Orig.GetViewSizes()
 	local ssx,ssy,spx,spy = Spring.GetScreenGeometry()
-	if (vsx > ssx or vsy > ssy) and Spring.SendCommands then
-		if tonumber(Spring.GetConfigInt("Fullscreen",1) or 1) == 1 then
-			Spring.SendCommands("Fullscreen 0")
-		else
-			Spring.SendCommands("Fullscreen 1")
+	if (vsx > ssx or vsy > ssy)  then
+		if Spring.SendCommands ~= nil then
+			if tonumber(Spring.GetConfigInt("Fullscreen",1) or 1) == 1 then
+				Spring.SendCommands("Fullscreen 0")
+			else
+				Spring.SendCommands("Fullscreen 1")
+			end
 		end
 		Spring.SetConfigInt("XResolution", tonumber(ssx))
 		Spring.SetConfigInt("YResolution", tonumber(ssy))
 		Spring.SetConfigInt("XResolutionWindowed", tonumber(ssx))
 		Spring.SetConfigInt("YResolutionWindowed", tonumber(ssy))
-		if tonumber(Spring.GetConfigInt("Fullscreen",1) or 1) == 1 then
-			Spring.SendCommands("Fullscreen 0")
-		else
-			Spring.SendCommands("Fullscreen 1")
+		if Spring.SendCommands ~= nil then
+			if tonumber(Spring.GetConfigInt("Fullscreen",1) or 1) == 1 then
+				Spring.SendCommands("Fullscreen 0")
+			else
+				Spring.SendCommands("Fullscreen 1")
+			end
 		end
+		vsx,vsy = Spring.Orig.GetViewSizes()
 	end
 	if WG.Chobby and WG.Chobby.Configuration and WG.Chobby.Configuration.uiScale then
 		WG.Chobby.Configuration:SetConfigValue('uiScale', vsx / 1800)
@@ -43,5 +48,9 @@ end
 
 
 function widget:Initialize()
-	widget:ViewResize()
+	checkResolution()
+end
+
+function widget:Update()
+	--checkResolution()
 end
