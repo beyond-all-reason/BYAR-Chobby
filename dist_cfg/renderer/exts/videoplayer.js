2504a6874f2c3d416e982486ddc0c72321ae849b
icexuick <info@ixstudios.nl>
2020-11-05 23:55:59 +0100

    Video Fix
    Sorry - Late + Beers

--- dist_cfg/renderer/exts/videoplayer.js
+++ dist_cfg/renderer/exts/videoplayer.js
@@ -4,5 +4,4 @@
 //const videos = ['endgame.mp4', 'craterstorm.mp4', 'redcomet.mp4', 'shorewaves.mp4', 'atmospherics.mp4', 'explosions.mp4', 'pyroattack.mp4'];
 const videos = ['endgame.mp4', 'craterstorm.mp4', 'pyroattack.mp4'];
 const video = videos[Math.floor(Math.random() * videos.length)];
-const video = videos[Math.floor(Math.random() * videos.length)];
 document.getElementById('videoplayer').src = 'video/' + video;
