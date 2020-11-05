d93f4a168fb3b1fc3c95e9504d211e30e0e075da
icexuick <info@ixstudios.nl>
2020-11-05 21:25:18 +0100

    BG Video fix

--- dist_cfg/renderer/exts/videoplayer.js
+++ dist_cfg/renderer/exts/videoplayer.js
@@ -1,6 +1,6 @@
 'use strict';
 
 //const videos = ['mayhem.mp4', 'dynamicwater.mp4', 'pyros.mp4', 'mayhem2.mp4', 'worktobedone.mp4', 'shorewaves.mp4'];
-const videos = ['endgame.mp4', 'craterstorm.mp4', 'redcomet', 'shorewaves.mp4', 'atmospherics.mp4', 'explosions.mp4', 'pyroattack.mp4'];
+const videos = ['endgame.mp4', 'craterstorm.mp4', 'redcomet.mp4', 'shorewaves.mp4', 'atmospherics.mp4', 'explosions.mp4', 'pyroattack.mp4'];
 const video = videos[Math.floor(Math.random() * videos.length)];
 document.getElementById('videoplayer').src = 'video/' + video;
