99d523e5f0413e3e07ca636c049f16d9e2b755a9
rlcevg <rlcevg@gmail.com>
2020-11-30 00:33:52 +0200

    Add faction selector

---
+++ LuaMenu/configs/gameConfig/byar/sidedata.lua
@@ -0,0 +1,16 @@
+local SIDEPICS_DIR = LUA_DIRNAME .. "configs/gameConfig/byar/sidepics/"
+
+return {
+	{
+		name = "Armada",
+		logo = SIDEPICS_DIR .. "armada.bmp",
+	},
+	{
+		name = "Cortex",
+		logo = SIDEPICS_DIR .. "cortex.bmp",
+	},
+	{
+		name = "Random",
+		logo = SIDEPICS_DIR .. "random.png",
+	},
+}
