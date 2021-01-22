93066195eb0ada5584b858a5e802a8c0221af8a4
Beherith <mysterme@gmail.com>
2021-01-22 12:28:04 +0100

    welcomepanel items update

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -3,24 +3,17 @@ local welcomePanelItems = {
         Header = "Welcome to BAR Alpha",
         Url = nil, --"https://www.beyondallreason.info/",
         Time = nil,-- "NowTime",
-        Text = "Welcome Commander! We have set up a single player skirmish for you, click start to jump right in!\n" .. 
-        "Right now you can enjoy single player skirmish with multiple AIs or multiplayer battles, join one of active rooms to play with the community.\n" .. 
-        "This lobby is currently being built, more functionality coming soon." ,
+        Text = "Welcome Commander! We have set up a single player skirmish for you, click start to jump right in, or customize it to your liking!\n" .. 
+        "Right now you can enjoy single player skirmish with multiple AIs or multiplayer battles, join one of active rooms to play with the community.\n" ,
     },
     {
-        Header = "Lobby Updates!",
+        Header = "Play with your friends: use our servers!",
         Url = nil,--"https://www.beyondallreason.info/",
         Time = nil,-- "NowTime",
-        Text = "Account recovery and start boxes on minimaps in progress!",
+        Text = "Join any empty room with your friends and type !boss to be able to !lock the game. Hosting your own private battles is under development.",
     },
     {
-        Header = "Play with your friends",
-        Url = nil,--"https://www.beyondallreason.info/",
-        Time = nil,-- "NowTime",
-        Text = "Join any empty room with your friends and type !boss to be able to !lock the game. Hosting your own battles is under development.",
-    },
-    {
-        Header = "Useful multiplayer battle room commands",
+        Header = "Multiplayer battle room commands",
         --Url = "https://www.beyondallreason.info/",
         --Time = "NowTime",
         Text = "Type these commands into the battle room chat\n" ..  
@@ -31,9 +24,9 @@ local welcomePanelItems = {
                "!preset coop - sets all players to one team and AIs to the other\n" ..
                "!fixcolors - Makes sure players dont have identical colors\n" ..
                "!forcestart - starts the game even if teams arent balanced or colors arent fixed\n" .. 
-               "!autobalance off - turns off automatic balancing (Recommended for PVE):\n" ..
-               "  !force Playername bonus [0-100] - Sets a % resource bonus for player or AI\n"..
-               "  !force Playername team 1 - Places player or AI into a specific team"
+               "!autobalance off - turns off automatic balancing (Recommended for PVE):\n" 
+               --"  !force Playername bonus [0-100] - Sets a % resource bonus for player or AI\n"..
+               --"  !force Playername team 1 - Places player or AI into a specific team"
                ,
     },
 }
