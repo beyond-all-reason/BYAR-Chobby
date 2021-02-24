f94322bb6d511ab28bd116d505451dfd6a91b117
Beherith <mysterme@gmail.com>
2021-02-24 10:58:51 +0100

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -6,8 +6,9 @@ return {
   mutator = 'Official',
   modtype = 5,
   onlyLocal = true,
-  depend = {
-      'rapid://chobby:test',
-      -- 'Chobby $VERSION',
+  depend = { 
+      -- For developing base chobby, switch out dependency
+      'rapid://chobby:test', --this uses rapid pinned chobby
+      --'Chobby $VERSION', -- this specifies chobby.sdd working path
   },
 }
