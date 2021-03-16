ef0872bcc2916e4aa85b720260d3956175ecba61
Beherith <mysterme@gmail.com>
2021-03-16 22:48:38 +0100

    Add taldarim v3 map

--- LuaMenu/configs/gameConfig/byar/mapDetails.lua
+++ LuaMenu/configs/gameConfig/byar/mapDetails.lua
@@ -65,6 +65,7 @@ return {
 ['Supreme_Crossing_V1']={Width=16, Height=16, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=1, Special=nil, Flat=1, Hills=nil, Water=1, IsInPool=1, },
 ['Tabula_Flooded_v04']={Width=16, Height=16, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=nil, Special=nil, Flat=nil, Hills=1, Water=1, IsInPool=1, },
 ['Tabula-v4']={Width=16, Height=16, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=nil, Special=nil, Flat=nil, Hills=1, Water=nil, IsInPool=1, },
+['Taldarim_V3']={Width=24, Height=18, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=1, Special=nil, Flat=nil, Hills=1, Water=nil, IsInPool=1, },
 ['Talus_v2']={Width=16, Height=16, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=1, Special=nil, Flat=nil, Hills=1, Water=nil, IsInPool=1, },
 ['Talus-wet-v3']={Width=16, Height=16, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=nil, Special=nil, Flat=nil, Hills=1, Water=1, IsInPool=1, },
 ['Tempest']={Width=20, Height=20, Is1v1=nil, IsTeam=1, IsFFA=nil, IsCertified=nil, Special=nil, Flat=1, Hills=nil, Water=1, IsInPool=1, },
