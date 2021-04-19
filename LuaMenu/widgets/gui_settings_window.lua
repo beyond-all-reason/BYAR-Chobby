75972f63857a8bfafff38e0c2c6694cecd33c454
Beherith <mysterme@gmail.com>
2021-04-19 16:42:33 +0200

    tooltip or die

--- LuaMenu/widgets/gui_settings_window.lua
+++ LuaMenu/widgets/gui_settings_window.lua
@@ -894,6 +894,7 @@ local function GetLobbyTabControls()
 		text = Configuration.serverAddress,
 		font = Configuration:GetFont(2),
 		useIME = false,
+		tooltip = "Requires a lobby restart for changes to take effect",
 		OnFocusUpdate = {
 			function (obj)
 				if obj.focused then
