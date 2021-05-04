a641910a69d7f6ac23d025b34bc2598e719609df
Damgam <c_2_damiano@op.pl>
2021-05-04 02:15:10 +0200

    new faction icons

--- LuaMenu/configs/gameConfig/byar/sidedata.lua
+++ LuaMenu/configs/gameConfig/byar/sidedata.lua
@@ -3,11 +3,11 @@ local SIDEPICS_DIR = LUA_DIRNAME .. "configs/gameConfig/byar/sidepics/"
 return {
 	{
 		name = "Armada",
-		logo = SIDEPICS_DIR .. "armada.bmp",
+		logo = SIDEPICS_DIR .. "armada.png",
 	},
 	{
 		name = "Cortex",
-		logo = SIDEPICS_DIR .. "cortex.bmp",
+		logo = SIDEPICS_DIR .. "cortex.png",
 	},
 	{
 		name = "Random",
