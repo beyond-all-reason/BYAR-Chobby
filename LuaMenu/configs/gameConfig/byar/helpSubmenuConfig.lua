a08b7b93a821a9790ef6824e66eb62062f34eec7
Damgam <c_2_damiano@op.pl>
2021-10-03 13:56:21 +0200

    replaced springfiles links with new domain

--- LuaMenu/configs/gameConfig/byar/helpSubmenuConfig.lua
+++ LuaMenu/configs/gameConfig/byar/helpSubmenuConfig.lua
@@ -89,7 +89,7 @@ local communityLines = {
 		"Browse and download maps.",
 		"Maps",
 		function ()
-			WG.BrowserHandler.OpenUrl("https://springfiles.com/spring/spring-maps")
+			WG.BrowserHandler.OpenUrl("https://springfiles.springrts.com/spring/spring-maps")
 		end
 	},
 	{
--- LuaMenu/configs/gameConfig/byar/linkFunctions.lua
+++ LuaMenu/configs/gameConfig/byar/linkFunctions.lua
@@ -7,7 +7,7 @@ local function link_replays()
 end
 
 local function link_maps()
-	return "https://springfiles.com/?type=1&filter=&category=map&tags="
+	return "https://springfiles.springrts.com/?type=1&filter=&category=map&tags="
 end
 
 return link_homePage, link_replays, link_maps
