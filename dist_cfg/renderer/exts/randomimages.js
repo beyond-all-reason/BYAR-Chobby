55f5341ff5b07e92aed51c3df5218c5e0725e878
Damgam <c_2_damiano@op.pl>
2021-12-26 03:26:01 +0100

    yet another test

--- dist_cfg/renderer/exts/randomimages.js
+++ dist_cfg/renderer/exts/randomimages.js
@@ -1,5 +1,2 @@
 'use strict';
 
-const images = ['1.png', '2.png', '3.png'];
-const image = images[Math.floor(Math.random() * images.length)];
-document.getElementById('randomimages').src = 'images/backgrounds/' + image;
