b02c0746f9d73dcab19e9cf4f1204654e445e000
Damgam <c_2_damiano@op.pl>
2021-12-26 05:08:37 +0100

    attempt to fix last commit

---
+++ dist_cfg/renderer/exts/random_background.js
@@ -0,0 +1,8 @@
+var images = [  './images/backgrounds/1.png',
+                './images/backgrounds/2.png',
+                './images/backgrounds/3.png',
+            ];        
+
+var imageName = images[Math.floor(Math.random() * images.length)];
+//console.log("ImageName:" +imageName);          
+document.getElementById("main-content").style.backgroundImage = "url("+imageName+")"; 
