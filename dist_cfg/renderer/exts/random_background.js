7249961d8c6c21b3caf7d8e30604b3ff5866e581
Damgam <c_2_damiano@op.pl>
2022-03-14 17:01:15 +0100

    some new launcher backgrounds ready for next release

--- dist_cfg/renderer/exts/random_background.js
+++ dist_cfg/renderer/exts/random_background.js
@@ -7,6 +7,15 @@ var images = [  './images/backgrounds/1.png',
                 './images/backgrounds/5.png',
                 './images/backgrounds/6.png',
                 './images/backgrounds/7.png',
+                './images/backgrounds/8.jpg',
+                './images/backgrounds/9.png',
+                './images/backgrounds/10.jpg',
+                './images/backgrounds/11.jpg',
+                './images/backgrounds/12.png',
+                './images/backgrounds/13.png',
+                './images/backgrounds/14.png',
+                './images/backgrounds/15.png',
+                './images/backgrounds/16.png',
             ];        
 
 var imageName = images[Math.floor(Math.random() * images.length)];
