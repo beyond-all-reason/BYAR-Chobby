1f76a6c9450afbad06656ec0365d8699556f96c2
Marek Rusinowski <marekrusinowski@gmail.com>
2022-03-16 18:37:22 +0100

    Reduce size of background images
    - Reduce size to be at least 1600x800 (launcher is 800x400 so it
      will look fine even on HiDPI monitors)
    - Reencode all images to avif with:
    
      $ avifenc -a tune=ssim --min 0 --max 63 -a end-usage=q \
        -a cq-level=22 --speed 2

--- dist_cfg/renderer/exts/random_background.js
+++ dist_cfg/renderer/exts/random_background.js
@@ -1,23 +1,9 @@
 'use strict';
 
-var images = [  './images/backgrounds/1.png',
-                './images/backgrounds/2.png',
-                './images/backgrounds/3.png',
-                './images/backgrounds/4.png',
-                './images/backgrounds/5.png',
-                './images/backgrounds/6.png',
-                './images/backgrounds/7.png',
-                './images/backgrounds/8.jpg',
-                './images/backgrounds/9.png',
-                './images/backgrounds/10.jpg',
-                './images/backgrounds/11.jpg',
-                './images/backgrounds/12.png',
-                './images/backgrounds/13.png',
-                './images/backgrounds/14.png',
-                './images/backgrounds/15.png',
-                './images/backgrounds/16.png',
-            ];        
-
+const images = [];
+for (let i = 1; i <= 16; ++i) {
+    images.push(`./images/backgrounds/${i}.avif`);
+}
 var imageName = images[Math.floor(Math.random() * images.length)];
 //console.log("ImageName:" +imageName);          
 document.getElementById("main-content").style.backgroundImage = "url("+imageName+")"; 
