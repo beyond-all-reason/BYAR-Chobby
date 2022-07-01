a1d328e5f50390ad8bd1bd6a95e0a9ac3bb4a003
MasterBel2 <untakenprefix@gmail.com>
2022-07-02 06:15:14 +1000

    Allow scaling adjustment; remember scale for each resolution (#202)

---
+++ LuaMenu/widgets/gfx_resize_scaling.lua
@@ -0,0 +1,17 @@
+function widget:GetInfo()
+	return {
+		name = "Resize Scaling",
+		desc = "Updates scaling on screen resize" ,
+		author = "Floris",
+		date = "",
+		license = "",
+		layer = 0,
+		enabled = true
+	}
+end
+
+function widget:ViewResize()
+    if WG.Chobby and WG.Chobby.Configuration then
+        WG.Chobby.Configuration:SetUiScale()
+    end
+end
