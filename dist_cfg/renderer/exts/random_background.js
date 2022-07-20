cf58d7d189d79c79e8647ce6a6f237b200fe9638
Marek Rusinowski <marekrusinowski@gmail.com>
2022-07-20 12:26:35 +0200

    Add two new launcher backgrounds from @Damgam

--- dist_cfg/renderer/exts/random_background.js
+++ dist_cfg/renderer/exts/random_background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const images = [];
-for (let i = 1; i <= 16; ++i) {
+for (let i = 1; i <= 18; ++i) {
     images.push(`./images/backgrounds/${i}.avif`);
 }
 var imageName = images[Math.floor(Math.random() * images.length)];
