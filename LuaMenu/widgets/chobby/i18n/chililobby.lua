38706f5b9bc71d73f5e64e26d64aa7629b849c84
The-Yak <lwagner5465@gmail.com>
2022-08-10 11:30:32 -0700

    Ready? for unready status

--- LuaMenu/widgets/chobby/i18n/chililobby.lua
+++ LuaMenu/widgets/chobby/i18n/chililobby.lua
@@ -204,7 +204,7 @@ return {
 
 		ready = "Ready",
 		ready_tooltip = "Click to become unready. This will prevent the game from starting!",
-		unready = "Ready Up!",
+		unready = "Ready?",
 		unready_tooltip = "Click to become ready. If you're not ready, the game can't start!",
 
 		-- chat_windows.lua
