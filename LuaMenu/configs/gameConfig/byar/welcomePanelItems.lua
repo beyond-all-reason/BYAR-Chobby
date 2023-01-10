c10c1aee2a21e1aaec19acb858849684d5d25305
Fireball <32598847+FIr3baL@users.noreply.github.com>
2023-01-11 00:18:29 +0100

    + images/1vs1Tournament.png

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -3,7 +3,8 @@ local welcomePanelItems = {
         Header = "Tournament: The Great Pro-Am Showdown",
         Url = "https://www.beyondallreason.info/news/the-great-pro-am-showdown",
         Time = "2023-01-14T15:00:00", -- "NowTime",
-        Text = "1v1 - 48 Players - Single Stage - Double Elimination\nPlayers of all skill levels welcome!\n",
+        Text = "1v1 - 48 Players - Single Stage - Double Elimination\nPlayers of all skill levels welcome!\n \n",
+        Image = "LuaMenu/images/1vs1Tournament.png",
         UrlText = "Sign-Up"
     },
     {
