b755364a4655da9992a2ab276845be63fe7e01c5
Gajo Petrovic <gajopetrovic@gmail.com>
2023-03-21 19:25:30 +0900

    split capture & replay into separate commands & update readme

---
+++ LuaMenu/widgets/dbg_command_replay.lua
@@ -0,0 +1,56 @@
+-- Sometimes this gets cached, hence the variable..
+local ENABLED = true
+local AUTO_QUIT_ON_FINISH = false
+local REPLAY_START_TIME = 1.0
+
+function widget:GetInfo()
+	return {
+		name = "Command replay",
+		desc = "Replays lobby commands",
+		author = "gajop",
+		date = "",
+		license = "",
+		layer = 99999,
+		enabled = ENABLED
+	}
+end
+
+if ENABLED then
+
+VFS.Include("libs/json.lua")
+local startClock
+
+function widget:Initialize()
+	Spring.Echo("===Command replay initialized===")
+	lobby = WG.LibLobby.lobby
+
+	startClock = os.clock()
+end
+
+local executed = false
+function widget:Update()
+	if executed then
+		if AUTO_QUIT_ON_FINISH then
+			Spring.Quit()
+		end
+		return
+	end
+
+	-- Give it some time to load the lobby before streaming commands
+	if os.clock() - startClock < REPLAY_START_TIME then
+		return
+	end
+
+	executed = true
+
+	if STREAM_COMMANDS then
+		cmds = json.decode(VFS.LoadFile("commands.json"))
+		Spring.Echo("Commands: " .. tostring(#cmds))
+
+		for i, v in ipairs(cmds) do
+			lobby:CommandReceived(v)
+		end
+	end
+end
+
+end
