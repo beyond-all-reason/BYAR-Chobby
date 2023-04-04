3f814ef8c204232b5318887c9298e1f01c858237
Beherith <mysterme@gmail.com>
2023-04-04 08:58:25 +0200

    Fix cache rapid pool

--- LuaMenu/widgets/api_rapid_pool_cache.lua
+++ LuaMenu/widgets/api_rapid_pool_cache.lua
@@ -16,45 +16,58 @@ local worktime = 0.1 -- in seconds per update
 local poolbasepath = "pool/"
 local poolDirs = {} -- ordered list of pool dirs
 local poolDirContents = {} -- table of pooldir to file contents, true for already managed, false for fresh
+local hexchars = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
+local thresholdHDD = 80 -- in MB/s 
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 -- Initialization
-local hexchars = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
 local current_index = 1
 local maxpools = 256
-local current_pool = "" 
+local current_pool = ""
 local VFS_RAW = VFS.RAW
 local loaded = 0
+local totaltime = 0
+local totalKB = 0
+local firstrun = true
+local garbage = 0
 
 function widget:Initialize()
 	Spring.Echo("Initializing Cache Rapid Pool")
-	local i = 1
+	local i = 0
 	for _,c1 in ipairs(hexchars) do 
 		for _,c2 in ipairs(hexchars) do 
+			i = i + 1
 			local pooldir = poolbasepath .. c1 ..c2 ..'/'
 			poolDirs[i] = pooldir
 			poolDirContents[pooldir] = false
-			i = i + 1
+			maxpools = i
 		end
 	end
 	current_pool = poolDirs[current_index]
+	--if VFS.FileExists(current_pool, VFS_RAW) ~= true then 
+	--	Spring.Echo("Unable to find rapid pool at", current_pool)
+	--	widgetHandler:RemoveWidget()
+	--end
 end
 
 function widget:Update()
 	local startTime = Spring.GetTimer()
 	local loadedNow = 0
+	local nowKB = 0
 	while (Spring.DiffTimers(Spring.GetTimer(), startTime) < worktime) do
 		if poolDirContents[current_pool] == false then -- fresh pool to work on	
 			poolDirContents[current_pool] = VFS.DirList(current_pool, '*.gz',VFS_RAW)
-			Spring.Echo("Adding dir to work", current_pool, #poolDirContents[current_pool])
+			--Spring.Echo("Adding dir to work", current_pool, #poolDirContents[current_pool])
 		else
 			local nextfile = next(poolDirContents[current_pool])
 			if nextfile then 
 				loaded = loaded + 1
-				VFS.LoadFile(nextfile)
-				poolDirContents[current_pool][nextfile] = nil 
-				loadedNow = loadedNow + 1 
+				local data = VFS.LoadFile(poolDirContents[current_pool][nextfile], VFS_RAW)
+				nowKB = nowKB + string.len(data) / 1024
+				garbage = garbage + nowKB
+				poolDirContents[current_pool][nextfile] = nil
+				loadedNow = loadedNow + 1
 			else
 				poolDirContents[current_pool] = true -- clears the table
 				-- increment pool counter
@@ -70,6 +83,25 @@ function widget:Update()
 			end
 		end
 	end
-	Spring.Echo("Pool Cacher loaded ", loadedNow, "files total", loaded)
+
+	if garbage > 300000 then
+		collectgarbage("collect")
+		garbage = 0
+	end
+
+	local realTime = Spring.DiffTimers(Spring.GetTimer(), startTime)
+	totaltime = totaltime + realTime
+	totalKB = totalKB + nowKB
+	Spring.Echo(string.format("Pool Cacher loaded %d files (%d KB, %.2f MB/s) in %.2fs", loadedNow, nowKB, 0.001*nowKB/realTime, realTime))
+	if firstrun then
+		if (0.001*nowKB/realTime) < thresholdHDD then
+			Spring.Echo("Cache Rapid Pool: Your load times are low, advise pre-caching")
+		end
+		firstrun = false
+	end
+
 end
 
+function widget:Shutdown()
+	Spring.Echo(string.format("Pool Cacher Done with %d files (%d KB, %.2f MB/s) in %.2fs", loaded, totalKB, 0.001*totalKB/totaltime,  totaltime))
+end
