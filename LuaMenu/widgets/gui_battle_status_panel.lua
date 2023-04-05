7d8392f9145ac70ab21976ba5832845e7fb8ea92
Fireball <32598847+FIr3baL@users.noreply.github.com>
2023-04-05 16:35:30 +0200

    simplify

--- LuaMenu/widgets/gui_battle_status_panel.lua
+++ LuaMenu/widgets/gui_battle_status_panel.lua
@@ -231,13 +231,11 @@ local function GetBattleInfoHolder(parent, battleID)
 		if isSpectator then
 			if queuePos and queuePos > 0 then
 				lblPlayerStatus:SetCaption("Join-Queue")
-				imPlayerStatus.file = IMG_STATUS_SPECTATOR
-				imPlayerStatus:Invalidate()
 			else
 				lblPlayerStatus:SetCaption("Spectator")
-				imPlayerStatus.file = IMG_STATUS_SPECTATOR
-				imPlayerStatus:Invalidate()
 			end
+			imPlayerStatus.file = IMG_STATUS_SPECTATOR
+			imPlayerStatus:Invalidate()
 		else
 			lblPlayerStatus:SetCaption("Player")
 			imPlayerStatus.file = IMG_STATUS_PLAYER
