b8e2f1769caadd2ebeef301f2a9acec1c698d284
Nikuksis <54234096+Nikuksis@users.noreply.github.com>
2023-04-12 23:08:46 +0300

    added center command map

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -32,15 +32,15 @@ local welcomePanelItems = {
     },
     
     {
-        Header = "New Map: High Noon Remake 1.0",
-        Image = "LuaMenu/configs/gameConfig/byar/minimapThumbnail/High_Noon_Remake_1.0.png",
-        Text = "New remake of the classic map by Phalange.",
+        Header = "New Map: Center Command BAR v1.0",
+        Image = "LuaMenu/configs/gameConfig/byar/minimapThumbnail/center_command_bar_v1.0.png",
+        Text = "A new life for an old classic\nBy Nikuksis.",
     },    
     
     {
-        Header = "New Map: Sphagnum Bog v1.1",
-        Image = "LuaMenu/configs/gameConfig/byar/minimapThumbnail/sphagnum_bog_v1.1.png",
-        Text = "New 1v1 - 3v3 map by Nikuksis.",
+        Header = "New Map: High Noon Remake 1.0",
+        Image = "LuaMenu/configs/gameConfig/byar/minimapThumbnail/High_Noon_Remake_1.0.png",
+        Text = "New remake of the classic map by Phalange.",
     },    
 
     {
