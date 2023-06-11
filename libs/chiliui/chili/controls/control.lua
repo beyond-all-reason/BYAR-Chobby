7d4a1305f8499ad138994915b5c8f8a835b370ac
Beherith <mysterme@gmail.com>
2023-06-11 18:44:58 +0200

    show where controls are allocated from when tracy is on

--- libs/chiliui/chili/controls/control.lua
+++ libs/chiliui/chili/controls/control.lua
@@ -179,6 +179,11 @@ function Control:New(obj)
 		end
 	end
 	obj:Realign()
+	if tracy then
+		local infostr, functionstr, arguments = Spring.Utilities.TraceEchoStr()
+		obj.allocatedfrom = functionstr
+		tracy.Message("Control:New:"..obj.name.." "..functionstr)
+	end
 
 	if WG.ChiliRedraw then
 		WG.ChiliRedraw.AddControl(obj, "New")
