f0c353b4b1c9435aedc99c3d691554e1ef4ffbde
Fireball <32598847+FIr3baL@users.noreply.github.com>
2023-08-29 22:33:55 +0200

    shorten values of modoptions, if longer than 32 characters (they can easily go up to 28000 , which is slowing down chobby to 1 fps for some reason)

--- LuaMenu/widgets/gui_modoptions_panel.lua
+++ LuaMenu/widgets/gui_modoptions_panel.lua
@@ -508,7 +508,8 @@ local function InitializeModoptionsDisplay()
 		modoptions = modoptions or {}
 		for key, value in pairs(modoptions) do
 			if modoptionDefaults[key] == nil or modoptionDefaults[key] ~= value then
-				text = text .. "\255\120\120\120" .. tostring(key) .. " = \255\255\255\255" .. tostring(value) .. "\n"
+				local shortenedValue = string.sub(tostring(value),1,)
+				text = text .. "\255\120\120\120" .. tostring(key) .. " = \255\255\255\255" .. shortenedValue .. "\n"
 				empty = false
 			end
 		end
