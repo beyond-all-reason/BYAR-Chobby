ceceeccca355b7a711ea9d5af4d84fd92a7c52bf
Moose <124457076+AntlerForce@users.noreply.github.com>
2023-12-01 19:23:02 -0700

    Remove separator from right click menu of AIs

--- LuaMenu/widgets/api_user_handler.lua
+++ LuaMenu/widgets/api_user_handler.lua
@@ -202,7 +202,7 @@ local function GetUserComboBoxOptions(userName, isInBattle, control, showTeamCol
 	if not (itsme or bs.aiLib) then																					comboOptions[#comboOptions + 1] = "Message" end
 	if isInBattle and not (itsme or bs.aiLib or info.isBot) then													comboOptions[#comboOptions + 1] = "Ring" end
 	if not (itsme or bs.aiLib or isInBattle) and info.battleID and validEngine then									comboOptions[#comboOptions + 1] = "Join Battle" end
-	if not (itsme or info.isBot) then																				comboOptions[#comboOptions + 1] = "\255\120\120\120" .. "--------------" end
+	if not (itsme or bs.aiLib or info.isBot) then																	comboOptions[#comboOptions + 1] = "\255\120\120\120" .. "--------------" end
 	if not (itsme or bs.aiLib or info.isBot) then																	comboOptions[#comboOptions + 1] = info.isFriend and "Unfriend" or "Friend"
 									  if info.isDisregarded and info.isDisregarded == Configuration.IGNORE then     comboOptions[#comboOptions + 1] = "Unignore"
 																													comboOptions[#comboOptions + 1] = "Avoid"
