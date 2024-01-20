8640176b2136c7a3c5d26c42ae7034fb5a38ab81
Beherith <mysterme@gmail.com>
2024-01-20 23:09:25 +0100

    Add EU6 too

--- LuaMenu/widgets/chobby/components/battle/battle_list_window.lua
+++ LuaMenu/widgets/chobby/components/battle/battle_list_window.lua
@@ -1212,6 +1212,7 @@ function BattleListWindow:OpenHostWindow()
 		['[teh]clusterEU3'] = {limit = 30, current = 0, online = false, region = 'EU'},
 		['[teh]clusterEU4'] = {limit = 150, current = 0, online = false, region = 'EU'}, 
 		['[teh]clusterEU5'] = {limit = 150, current = 0, online = false, region = 'EU'},
+		['[teh]clusterEU6'] = {limit = 100, current = 0, online = false, region = 'EU'},
 		['[teh]clusterUS'] = {limit = 70, current = 0, online = false, region = 'US'},
 		['[teh]clusterUS2'] = {limit = 30, current = 0, online = false, region = 'US'},
 		['[teh]clusterUS3'] = {limit = 70, current = 0, online = false, region = 'US'},
