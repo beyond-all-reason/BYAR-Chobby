e5886eb401e8f0c35d0c7d9f5f48d941ae06b7d9
alwaysnicki <157929850+alwaysnicki@users.noreply.github.com>
2024-02-01 14:07:09 -0700

    Add files via upload

--- LuaMenu/configs/gameConfig/byar/scenarios/scenario022.lua
+++ LuaMenu/configs/gameConfig/byar/scenarios/scenario022.lua
@@ -8,26 +8,26 @@ local scenariodata = {
 	imagepath		= "scenario022.jpg", -- placed next to lua file, should be 3:1 ratio banner style
 	imageflavor		= "River Assault", -- This text will be drawn over image
     summary         = [[After a ferocious battle, a Cortex Commander decided to join Armada forces. Unfortunately, other Cortex commanders discovered the intent of the treacherous plan and sent a mighty force to prevent it.]],
-	briefing 		= [[You will start with some Lazarus (Resurrection and Repair Bots), that can resurrect units from their wrecks. Your only hope is to hide your position and build a mighty army to crush all opposing forces.
+	briefing 		= [[You will start with some Lazarus Bots, that can resurrect units from their wrecks. Your only hope is to hide your position and build a mighty army to crush all opposing forces.
        
 	!!! DO NOT UNDERESTIMATE YOUR ENEMY !!!
       
-  While you will be fighting for your life, don't forget to fortify your Ally too, since we can't lose the Cortex Command Tower. 
+  While you will be fighting for your life, don't forget to fortify your ally too, since we can't lose the Cortex Command Tower. 
   
 Reinforcements:
- - You and your Ally will receive the first contingency around the 9th minute from the beginning of mission.
- - The enemy will be getting Reinforcements every 8 and 20 minutes.
+ - You and your ally will receive the first contingency around the 9th minute from the beginning of the mission.
+ - The enemy will be getting reinforcements every 8 and 20 minutes.
     
 Tips:
- - To gain access to your Ally's units, build Decoy Commander and capture one of your Ally's Constructor units. 
- - Use Radar Jamming units to hide your presence from enemy Radar.
+ - To gain access to your Ally's units, build a Decoy Commander and capture one of your ally's constructor units. 
+ - Use radar jamming units and structures to hide your presence from enemy radar.
  - Resurrection bots will use Energy to resurrect units, at a flat cost of 75e per second while resurrecting.
- - You can issue Area-Resurrect and Area-Reclaim commands by right-click dragging.
+ - You can issue Resurrect Area and Reclaim Area commands by right-click dragging after selecting the corresponding command (either through the menu in the lower left or the key assigned to the action).
  - Repairing units does not cost any resources.
      
 Scoring:
  - Time taken to finish the scenario
- - Resources spent to complete victory condition
+ - Resources spent to complete the victory condition
      
 											     IMPORTANT CHANGE!!
 	Beginner: Your Resources = +50%, Enemy Resources = -20%
