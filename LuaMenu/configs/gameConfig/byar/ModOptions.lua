4a45261a75ee3dcb909d289a8991790006dd0c08
Timothy Verbruggen <73989722+Avaristimo@users.noreply.github.com>
2024-02-13 23:07:32 +0100

    Update ModOptions.lua

--- LuaMenu/configs/gameConfig/byar/ModOptions.lua
+++ LuaMenu/configs/gameConfig/byar/ModOptions.lua
@@ -899,12 +899,12 @@ local options={
 	},
 	
 	{
-		key = "skyshift",
-		name = "Skyshift: Air Rework",
-		desc = "A complete overhaul of air units and mechanics",
-		type = "bool",
+		key = 'skyshift',
+		name = 'Skyshift: Air Rework',
+		desc = 'A complete overhaul of air units and mechanics',
+		type = 'bool',
 		def = false,
-		section = "options_experimental",
+		section = 'options_experimental',
 		hidden = true,
 	},
 
