7518494c488e83c9dbf4393f8e8983ceb21418eb
Beherith <mysterme@gmail.com>
2024-04-19 12:50:25 +0200

    fix broken chobby version

--- libs/liblobby/lobby/interface.lua
+++ libs/liblobby/lobby/interface.lua
@@ -32,15 +32,15 @@ function Interface:Register(userName, password, email)
 end
 
 local function GetLobbyName()
-	local byarchobbyrapidTag = 'byar-chobby:test'
-	if byarchobbyrapidTag and VFS.GetNameFromRapidTag then
-		local rapidName = VFS.GetNameFromRapidTag(byarchobbyrapidTag)
-		if rapidName then
-			byarchobbyrapidTag = rapidName
-			byarchobbyrapidTag = string.gsub(byarchobbyrapidTag, "BYAR Chobby test%-", "")
+	local byarchobbyrapidTag = "unknown"
+	for i,v in ipairs(VFS.GetLoadedArchives()) do 
+			if string.find(v,"BYAR Chobby ", nil, true) then
+			byarchobbyrapidTag = string.gsub(string.gsub(v,"test%-", ""), "BYAR Chobby ", "")
+			byarchobbyrapidTag = string.gsub(byarchobbyrapidTag, "[^%w]", " ")
+			break
 		end
 	end
-	local lobbyname = 'Chobby:'..byarchobbyrapidTag
+	local lobbyname = 'Chobby '..byarchobbyrapidTag
 	--Spring.Utilities.TraceFullEcho()
 	return lobbyname
 end
