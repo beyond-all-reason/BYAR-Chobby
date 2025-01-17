b81d1ba2f56ce696bc8000d1517ee92995f10f9b
Moose <124457076+AntlerForce@users.noreply.github.com>
2025-01-17 16:23:08 -0700

    "Medium" = normal for pve

--- LuaMenu/configs/gameConfig/byar/singleplayerQuickSkirmish.lua
+++ LuaMenu/configs/gameConfig/byar/singleplayerQuickSkirmish.lua
@@ -62,13 +62,19 @@ function skirmishSetupData.ApplyFunction(battleLobby, pageChoices)
 	})
 
 	-- Handle PvE modes
+	local difficultyMap = {
+		["Easy"] = "easy",
+		["Medium"] = "normal",  -- Map "Medium" to "normal" for PvE
+		["Hard"] = "hard"
+	}
+
 	if gameType == 4 then -- Scavengers
 		battleLobby:AddAi("ScavengersDefenseAI(1)", "ScavengersAI", 1)
-		battleLobby:SetModOptions({scav_difficulty = string.lower(pageConfig[2].options[difficulty])})
+		battleLobby:SetModOptions({scav_difficulty = difficultyMap[pageConfig[2].options[difficulty]]})
 		return
 	elseif gameType == 5 then -- Raptors
 		battleLobby:AddAi("RaptorsDefenseAI(1)", "RaptorsAI", 1)
-		battleLobby:SetModOptions({raptor_difficulty = string.lower(pageConfig[2].options[difficulty])})
+		battleLobby:SetModOptions({raptor_difficulty = difficultyMap[pageConfig[2].options[difficulty]]})
 		return
 	end
 
