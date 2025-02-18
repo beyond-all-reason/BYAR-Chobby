e7ac3e344e99a44e4db1145ff3fd505a8f43acd9
MasterBel2 <untakenprefix@gmail.com>
2025-02-18 16:13:21 +1000

    Remove indirection

--- LuaMenu/widgets/chobby/components/battle/battle_list_window.lua
+++ LuaMenu/widgets/chobby/components/battle/battle_list_window.lua
@@ -58,10 +58,6 @@ function BattleListWindow:init(parent)
 		}
 	end
 
-	local function update()
-		self:Update()
-	end
-
 	self.infoPanel = Panel:New {
 		classname = "overlay_window",
 		x = "15%",
@@ -259,7 +255,7 @@ function BattleListWindow:init(parent)
 
 	local function onConfigurationChange(listener, key, value)
 		if key == "displayBadEngines2" then
-			update()
+			self:Update()
 		elseif key == "battleFilterRedundant" then
 			self:SoftUpdate()
 		end
@@ -273,7 +269,7 @@ function BattleListWindow:init(parent)
 	end
 	WG.DownloadHandler.AddListener("DownloadFinished", downloadFinished)
 
-	update()
+	self:Update()
 end
 
 function BattleListWindow:RemoveListeners()
