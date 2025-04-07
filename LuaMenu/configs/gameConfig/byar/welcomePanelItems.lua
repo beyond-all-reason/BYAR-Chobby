b2e4cf1cd9021e9cf916fd4686fcc5c92294138f
BAR-Neb <maxpliskin@outlook.com>
2025-04-07 15:52:14 -0600

    Update welcomePanelItems.lua

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -36,7 +36,7 @@ local welcomePanelItems = {
         Header = "BAR Academy Live! - 1v1 Replay Reviews",
         Text = "1v1 Replay reviews with BARCast and neb_",
         Time = "2025-04-08T17:00:00",
-        Url = "https://twitch.tv/BeyondAll",
+        Url = "https://twitch.tv/BeyondAllReasonOfficial",
         UrlText = "Watch Here!",
     },
     {
