8b15224fceb81eb6040932c4c1395440cca20123
BAR-Neb <maxpliskin@outlook.com>
2025-04-14 16:30:24 -0600

    Update welcomePanelItems.lua

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -16,7 +16,7 @@ local welcomePanelItems = {
     },
     {
         Header = "Alpha Cup V 2025",
-        Text =" \n" .. "Congratulations to our new 1v1 Champion - Autopilot! In case you missed it, you can still rewatch the Alpha Cup madness on YouTube." .. " \n" .. " \n" .. "Thank you to everyone in the community for making this event happen! At 87 players, this was the biggest Alpha Cup yet!",
+        Text =" \n" .. "Congratulations to our new 1v1 Champion - Autopilot! In case you missed it, you can still rewatch the Alpha Cup madness on YouTube." .. " \n" .. " \n" .. "Thank you to everyone in the community for making this event happen! At 87 players, this was the biggest Alpha Cup yet.",
         Image = "LuaMenu/images/AlphaCupV.png",
         Url = "https://www.youtube.com/watch?v=u-6QLG2leNs&t=3s",
         UrlText = "Watch the Finals",
