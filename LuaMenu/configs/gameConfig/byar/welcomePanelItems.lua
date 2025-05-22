432d59a841da0853440981c78f535cedbb9c6b01
NebuchadnezzarII <maxpliskin@outlook.com>
2025-05-22 12:29:02 -0600

    Faction wars main stage announcement (#1001)

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -15,21 +15,21 @@ local welcomePanelItems = {
         Text = "Welcome back Commander. We hope you are ready for epic Singleplayer and Multiplayer battles. Check out our Discord and join the community!\n",
     },
 
-    --{
-       -- Header = "Faction Wars Main Stage",
-       -- Time = "2025-05-24T15:00:00",
-       -- Text = "It's time! Tune in to the Faction Wars Main Stage as the 8 qualifying teams compete for 4 spots in the semi-finals",
-        --Image = "LuaMenu/images/FactionWars3v3.png",
-        --Url = "https://www.twitch.tv/beyondallreasonofficial",
-        --UrlText = "Watch Here",
-    --},
-
     {
         Header = "The Lightbringer Update is Here!",
         Text = "Commanders, after two weeks of a testing run for the Lightbringer Update, we are thrilled to announce the merge went without any major issues. This update represents the largest visual overhaul we have ever attempted. Enjoy BAR battles being more lit than ever!" .. " \n" .. " \n" .. "We have prepared a detailed post about the update and a Cinematic Highlights Video, check it out!",
-		    Image = "LuaMenu/images/welcomepanel/lightbringerbox.png",
+	    Image = "LuaMenu/images/welcomepanel/lightbringerbox.png",
         Url = "https://www.beyondallreason.info/news/lightbringer-update",
         UrlText = "Read the Full Changes",
+    },
+
+    {
+        Header = "Faction Wars: Main Stage!",
+        Time = "2025-05-24T15:00:00",
+        Text = "Catch the qualifying 8 teams go head to head this weekend as they fight over four spots in the Semi Finals!"  .. " \n" .. " \n" ..  "Casted by Lostdeadman, Hells Hound, Angry Strawberry, vdb and more!",
+        Image = "LuaMenu/images/FactionWars3v3.png",
+        Url = "https://www.youtube.com/watch?v=2juI32sre1o",
+        UrlText = "Watch The Qualifier",
     },
 
     {
