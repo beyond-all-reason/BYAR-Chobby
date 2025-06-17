bb1a654305ae5826f4f06408ced987ca9135a8e3
Nicolas Busseneau <nicolas@busseneau.fr>
2025-06-17 15:37:59 +0200

    announce Nation Wars knockout stage (#1013)

--- LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
+++ LuaMenu/configs/gameConfig/byar/welcomePanelItems.lua
@@ -24,6 +24,14 @@ local welcomePanelItems = {
     },
 
     {
+        Header = "Nation Wars #4 – Knockout stage",
+        Text = "16 teams entered the group stage last weekend and battled it out in 4v4, with the best 8 now advancing to the knockout stage.\nWill the United States defend their title, or will another nation claim it?".." \n".." \n".."Tune in to Twitch this weekend June 21/22 and watch the remaining competitors fight their way through the brackets.\nFor each team, the only acceptable outcome is the complete elimination of the others.".." \n".." \n".."Full details and stream links available on the Nation Wars Discord.",
+        Image = "LuaMenu/images/nation_wars.png",
+        Url = "https://discord.gg/qurXWcjmNN",
+        UrlText = "Nation Wars Discord",
+    },
+
+    {
         Header = "Help Keep BAR Clean - Join the Overwatch Team!",
         Text = "As BAR grows, so does the need for solid community moderation. We're looking for reliable, level-headed players to join the Overwatch Team—a trusted group that helps review reports, flag issues, and keep things fair.".." \n".." \n".."If you’re active and care about a respectful game space, we’d love your help!".." \n".." \n".."Grab the development role from ⁠Channels & Roles to access the thread below:",
         Image = "LuaMenu/images/overwatch.png",
