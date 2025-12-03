3f0c0b113556f20a8737c6db5b064fa7593377c0
Oliwier <59885012+robertthepie@users.noreply.github.com>
2025-12-03 14:54:23 +0000

    Add links (for linking feedback threads/forms) to modoptions panel. (#1081)
    and also option to hide options after certain unix timestamp

--- LuaMenu/widgets/gui_modoptions_panel.lua
+++ LuaMenu/widgets/gui_modoptions_panel.lua
@@ -545,6 +545,43 @@ local function ProcessSubHeader(data, index)
 	return label
 end
 
+local function ProcessLinkButton(data, index)
+	local button = Button:New {
+		x = 5,
+		y = index * 32,
+		width = data.width or 150,
+		height = 30,
+		valign = "center",
+		align = "left",
+		caption = data.name,
+		objectOverrideFont = WG.Chobby.Configuration:GetFont(tonumber(data.font) or 2),
+		tooltip = data.link,
+		padding = {2, 2, 2, 2},
+		OnMouseUp = {
+			function()
+				WG.Chobby.ConfirmationPopup(
+					function() WG.BrowserHandler.OpenUrl(data.link) end,
+						"\255\255\128\128".."External Website\nOpen in your web browser?"
+						..	(data.desc and "\n \n"..data.desc or "")
+						..	("\n \n"..data.link),
+					nil, nil, nil, "Open Link", "Cancel", nil)
+			end
+		}
+	}
+	local externalWebsite = Image:New {
+		name = 'externalWebsite',
+		x = button.width-25,
+		y = 0,
+		right = 0,
+		bottom = 0,
+		keepAspect = true,
+		file = LUA_DIRNAME .. "images/external-website.png",
+		parent = button,
+		tooltip = button.tooltip,
+	}
+	return button
+end
+
 local function ProcessLineSeparator(data, index)
 	return Line:New {
 		x = 0,
@@ -596,7 +633,8 @@ local function PopulateTab(options)
 			elseif data.type == "separator" then
 				rowData = ProcessLineSeparator(data, row)
 				row = row - 0.5
-
+			elseif data.type == "link" and data.link then
+				rowData = ProcessLinkButton(data, row)
 			end
 			if rowData then
 				column = math.abs(data.column or 1)
@@ -1052,6 +1090,8 @@ function ModoptionsPanel.LoadModoptions(gameName, newBattleLobby)
 		return
 	end
 
+	local currentUnixTime = os.time()
+
 	-- Set modoptionDefaults
 	for i = 1, #modoptions do
 		local data = modoptions[i]
@@ -1064,6 +1104,9 @@ function ModoptionsPanel.LoadModoptions(gameName, newBattleLobby)
 			else
 				modoptionDefaults[data.key] = tostring(data.def)
 			end
+		end
+		if data.hidden and type(data.hidden) == "number" then
+			data.hidden = currentUnixTime > data.hidden
 		end
 	end
 
