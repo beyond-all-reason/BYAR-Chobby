cd476357923697223c18db8ff553f74b9b1f3c88
Moose <124457076+AntlerForce@users.noreply.github.com>
2025-12-25 17:18:17 -0700

    Update ATG version in Simple Skirmish

--- LuaMenu/configs/gameConfig/byar/singleplayerQuickSkirmish.lua
+++ LuaMenu/configs/gameConfig/byar/singleplayerQuickSkirmish.lua
@@ -33,13 +33,13 @@ local mapsByGameType = {
 	["Scavengers"] = {
 		"Ancient Bastion Remake 0.5",
 		"Ancient Vault v1.4",
-		"All That Glitters v2.2",
+		"All That Glitters v2.2.3",
 		"Requiem Outpost 1.0.1",
 	},
 	["Raptors"] = {
 		"Ancient Bastion Remake 0.5",
 		"Ancient Vault v1.4",
-		"All That Glitters v2.2",
+		"All That Glitters v2.2.3",
 		"Requiem Outpost 1.0.1",
 	},
 }
