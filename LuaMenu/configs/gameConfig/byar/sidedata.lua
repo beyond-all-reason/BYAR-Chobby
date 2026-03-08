b19cabfb01bf0276790ca71780569bd9d04832ff
PtaQQ <ptaszekpawel@gmail.com>
2026-03-08 13:51:26 +0100

    New Faction Logos and better alignment / sizing

--- LuaMenu/configs/gameConfig/byar/sidedata.lua
+++ LuaMenu/configs/gameConfig/byar/sidedata.lua
@@ -4,10 +4,12 @@ return {
 	{	--	0
 		name = "Armada",
 		logo = SIDEPICS_DIR .. "armada.png",
+		logoScale = 0.90,
 	},
 	{	--	1
 		name = "Cortex",
 		logo = SIDEPICS_DIR .. "cortex.png",
+		logoScale = 0.85,
 	},
 	{	--	2
 		name = "Random",
